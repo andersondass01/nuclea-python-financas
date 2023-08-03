9c66f5aa6c6097b876675522bcea7caa7f8b95d8
Anderson <anderson.dass@hotmail.com>
2023-08-02 21:40:13 -0300

    Modulo Python - Aula 02/08

---
+++ moldes(classes)/cliente.py
@@ -0,0 +1,9 @@
+class Cliente:
+
+    def __init__(self):
+        self.nome = None
+        self.cpf = None
+        self.rg = None
+        self.data_nascimento = None
+        self.cep = None
+        self.numero_casa = None

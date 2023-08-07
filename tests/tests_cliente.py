7cb1cb4a7fad72e4a1caf91dfb34ce6c08047662
Anderson <anderson.dass@hotmail.com>
2023-08-06 22:35:34 -0300

    Teste Python - 06/08

--- tests/tests_cliente.py
+++ tests/tests_cliente.py
@@ -3,13 +3,12 @@ from unittest.mock import patch
 
 from faker import Faker
 
-from main import main, clientes
+from main import main
+from repository.banco_de_dados import BancoDeDados
 from utils.valida_cpf import gera_cpf
 
 
 class TestStringMethods(unittest.TestCase):
-    pass
-
     def gerar_nome_fake(self):
         fake = Faker()
         return fake.name()
@@ -17,19 +16,20 @@ class TestStringMethods(unittest.TestCase):
     def test_cliente(self):
         nome = self.gerar_nome_fake()
         cpf = gera_cpf()
-        inputs = ["1", nome, cpf, "12.345.678-x", "12/12/2012", "86082147", "100", "não"]
+        inputs = ["1", "1", nome, cpf, "12.345.678-x", "12/12/2012", "86082147", "casa", "100", "5"]
 
         with patch("builtins.input", side_effect=inputs):
             main()
 
         cliente_esperado = {
             "nome": nome,
-            "cpf": f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:11]}",
-            "rg": "12.345.678-x",
-            "data_nascimento": "12/12/2012",
-            "endereco": {'CEP': '86082-147', 'Logradouro': 'Rua João Gil Ortega', 'Bairro': 'Parigot de Souza 2',
-                         'Cidade': 'Londrina', 'Estado': 'PR'},
-            "numero_casa": "100"
+            "cpf": f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}",
         }
 
-        self.assertIn(cliente_esperado, clientes)
+        consulta = BancoDeDados()
+        clientes = consulta.select(cliente_esperado)
+
+        cliente_esperado = consulta.select(cliente_esperado)
+
+        self.assertEquals(cliente_esperado, clientes)
+
